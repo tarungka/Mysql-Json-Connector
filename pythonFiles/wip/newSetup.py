32edddfc0ba75b73d5d59d3b0e14a5bb82d7c335
Tarun Gopalkrishna <tarungopalkrishna@gmail.com>
2019-02-11 17:11:50 +0530

    Updated the newSetup.py

--- pythonFiles/wip/newSetup.py
+++ pythonFiles/wip/newSetup.py
@@ -3,12 +3,12 @@ from mysql.connector import errorcode
 import query
 import json
 import import_logger
-import hashlib
-import jsonMysqlParser as parser
-from Crypto.Cipher import AES
-from Crypto import Random
-from Crypto.Random import get_random_bytes
-import sys
+#import hashlib
+#import jsonMysqlParser as parser
+#from Crypto.Cipher import AES
+#from Crypto import Random
+#from Crypto.Random import get_random_bytes
+#import sys
 logger =  import_logger.logIt(__file__)
 
 def main(jsonCnf):
@@ -28,14 +28,14 @@ def main(jsonCnf):
 	for database in databases:
 		try:
 			cursor.execute(query.useQuery(database))
-			#answer = input("Database %s is present, are you sure you want to drop it(Y/N)?" % (database))
-			#if(answer == 'y' or answer == 'Y'):
-			cursor.execute(query.dropQuery("database",database))
-			#	logger.log(database + " was dropped.",True)
-			#else:
-			#	logger.log("Please change the name of the database in the config file and try again.",True)
-			#	logger.log("Exitting",True)
-			#	exit(0)
+			answer = input("Database %s is present, are you sure you want to drop it(Y/N)?" % (database))
+			if(answer == 'y' or answer == 'Y'):
+				cursor.execute(query.dropQuery("database",database))
+				logger.log(database + " was dropped.",True)
+			else:
+				logger.log("Please change the name of the database in the config file and try again.",True)
+				logger.log("Exitting",True)
+				exit(0)
 		except:
 			logger.log(database + " does not exist,no need to drop")
 	allDatabases = databases.keys()
@@ -50,28 +50,28 @@ def main(jsonCnf):
 			dictionary = tableObject["tables"][table]["table_constrains"]
 			cursor.execute(query.createQuery("table",table,dictionary))
 	logger.log("All databases and tables are created",True)
-	parse = parser.jsonConfiguration("json/config_mysql.json")
-	allDatabases = parse.getDatabases()
-	allTables = parse.getTables()
-	jsonEncryption = {}
-	print(get_random_bytes(16))
-	encrypt = AES.new(get_random_bytes(16),AES.MODE_CBC,get_random_bytes(16))
-	for table in allTables:
-		print("Entering tables")
-		print(table)
-		print(encrypt.encrypt(table.ljust(16)))
-		ans = encrypt.encrypt(table.ljust(16))
-		jsonEncryption.update({str(ans) : table})
-		print("---",encrypt.decrypt(ans))
-		print(jsonEncryption)
-		print("----------------------------")
-		try: 
-			ans = encrypt.encrypt(table)
-			print(ans)
-		except:
-			print("Cannot print")
-	print(json.dumps(jsonEncryption))
-	parse.saveAs("json/newSetupJson.json",json.dumps(jsonEncryption,indent=4))
+	#parse = parser.jsonConfiguration("json/config_mysql.json")
+	#allDatabases = parse.getDatabases()
+	#allTables = parse.getTables()
+	#jsonEncryption = {}
+	#print(get_random_bytes(16))
+	#encrypt = AES.new(get_random_bytes(16),AES.MODE_CBC,get_random_bytes(16))
+	#for table in allTables:
+	#	print("Entering tables")
+	#	print(table)
+	#	print(encrypt.encrypt(table.ljust(16)))
+	#	ans = encrypt.encrypt(table.ljust(16))
+	#	jsonEncryption.update({str(ans) : table})
+	#	print("---",encrypt.decrypt(ans))
+	#	print(jsonEncryption)
+	#	print("----------------------------")
+	#	try: 
+	#		ans = encrypt.encrypt(table)
+	#		print(ans)
+	#	except:
+	#		print("Cannot print")
+	#print(json.dumps(jsonEncryption))
+	#parse.saveAs("json/newSetupJson.json",json.dumps(jsonEncryption,indent=4))
 	
 	
 
