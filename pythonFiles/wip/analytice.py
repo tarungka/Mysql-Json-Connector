f51e2e9a95c6775769f6a6d06ee62b2d0622b3e2
RAIL-SKSJTI <railatsk@gmail.com>
2019-02-19 13:23:05 +0530

    aslkdjf

--- pythonFiles/wip/analytice.py
+++ pythonFiles/wip/analytice.py
@@ -18,9 +18,14 @@ class analytics:
         print('updatedAttendece')
         self.cursor.execute("SELECT * FROM  students_attendence")
         fetchedData = self.cursor.fetchall()
-        print(fetchedData[1][3])
-        print(fetchedData[1][4])
-        print(fetchedData[1][4] - fetchedData[1][3])
+        for data in fetchedData:
+            if(data[4] != None):
+                #print(data[1],data[4] - data[3])
+                print("UPDATE attendence set time_spent="+str(data[4] - data[3])," where time_in='"+str(data[3])+"' and rail_id ='"+str(data[0])+"';")
+                self.cursor.execute("UPDATE attendence set time_spent=`"+str(data[4] - data[3]),"` where time_in ='"+str(data[3])+"' and rail_id ='"+str(data[0])+"';")
+        #print(fetchedData[1][3])
+        #print(fetchedData[1][4])
+        #print(fetchedData[1][4] - fetchedData[1][3])
     def updatedStudents(self):
         print("updatedStudents")
 
