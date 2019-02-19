537ecba8db49107a01aef3b230ff019d1fabe5d3
RAIL-SKSJTI <railatsk@gmail.com>
2019-02-19 15:53:46 +0530

    made the analytics.py

--- pythonFiles/wip/analytice.py
+++ pythonFiles/wip/analytice.py
@@ -10,19 +10,32 @@ class analytics:
             host = "localhost",
             user = "testuser",
             passwd = "testuser",
-            database = "tdb"
+            database = "railDB"
         )
         self.cursor = self.connection.cursor()
         print("Constructor")
     def updatedAttendece(self):
         print('updatedAttendece')
-        self.cursor.execute("SELECT * FROM  students_attendence")
+        self.cursor.execute("SELECT * FROM  attendence")
         fetchedData = self.cursor.fetchall()
+        #for data in fetchedData:
+        #    if(data[4] != None):
+        #        #print(data[1],data[4] - data[3])
+        #        print              ("UPDATE attendence set time_spent='"+str(data[4] - data[3])+"' where time_in='"+str(data[3])+"' and rail_id ='"+str(data[0])+"';")
+        #        self.cursor.execute("UPDATE attendence set time_spent='"+str(data[4] - data[3])+"' where time_in='"+str(data[3])+"' and rail_id ='"+str(data[0])+"';")
+        dictionary = {}
         for data in fetchedData:
-            if(data[4] != None):
-                #print(data[1],data[4] - data[3])
-                print("UPDATE attendence set time_spent="+str(data[4] - data[3])," where time_in='"+str(data[3])+"' and rail_id ='"+str(data[0])+"';")
-                self.cursor.execute("UPDATE attendence set time_spent=`"+str(data[4] - data[3]),"` where time_in ='"+str(data[3])+"' and rail_id ='"+str(data[0])+"';")
+            if(data[0] in list(dictionary.keys())):
+                if(data[4] != None):
+                    #print(data,"update to",dictionary[data[0]])
+                    print("UPDATE attendence SET time_since_last_login='"+dictionary[data[0]].strftime('%Y-%m-%d %H:%M:%S')+"' WHERE rail_id='"+data[0]+"' AND time_in='",str(data[3]),"';")
+                    self.cursor.execute("UPDATE attendence SET time_since_last_login='"+dictionary[data[0]].strftime('%Y-%m-%d %H:%M:%S')+"' WHERE rail_id='"+data[0]+"' AND time_in='",str(data[3]),"';")
+            dictionary.update({data[0] : data[3]})
+        #print(dictionary)
+        #print(len(dictionary.keys()))
+        #print(data[1])
+        self.connection.commit()
+        #print("WORKING")
         #print(fetchedData[1][3])
         #print(fetchedData[1][4])
         #print(fetchedData[1][4] - fetchedData[1][3])
